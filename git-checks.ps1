fde57725d6ad42221a1b07dd976471bcb13e05cd
Bernie White <bewhite@microsoft.com>
2021-04-10 00:04:38 +1000

    fetch head

--- git-checks.ps1
+++ git-checks.ps1
@@ -1,3 +1,11 @@
 
-git diff --name-only main...HEAD
+"---"
+"git diff --name-only main...FETCH_HEAD"
+git diff --name-only main...FETCH_HEAD
+
+"---"
+"git diff --name-only main...HEAD~1"
+git diff --name-only main...HEAD~1
+
+"---"
 
