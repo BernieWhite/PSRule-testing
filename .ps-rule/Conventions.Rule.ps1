3940f7382180e3a73c60234a83d5c71468d38765
Bernie White <bewhite@microsoft.com>
2021-05-20 20:20:28 +1000

    Updated convention processing order

--- .ps-rule/Conventions.Rule.ps1
+++ .ps-rule/Conventions.Rule.ps1
@@ -1,6 +1,7 @@
 
 
-Export-PSRuleConvention 'GH-Hook' -If { ![String]::IsNullOrEmpty($Env:GITHUB_REPOSITORY) } {
+Export-PSRuleConvention 'GH-Hook' -If { ![String]::IsNullOrEmpty($Env:GITHUB_REPOSITORY) } -Begin {
+    Write-Warning "Running convention"
     Export-GitHubRuleData -UseGitHubToken -OutputPath 'out/repo/repo.json';
 
     $PSRule.Import($PSrule.GetContent('out/repo/repo.json'));
