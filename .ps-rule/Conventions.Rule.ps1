3972faae71d2f4bb62717b2ef082ef62b5dcc3de
Bernie White <bewhite@microsoft.com>
2021-05-20 20:43:05 +1000

    Update convention

--- .ps-rule/Conventions.Rule.ps1
+++ .ps-rule/Conventions.Rule.ps1
@@ -1,10 +1,14 @@
 
 
-Export-PSRuleConvention 'GH-Hook' -If { ![String]::IsNullOrEmpty($Env:GITHUB_REPOSITORY) } -Begin {
+Export-PSRuleConvention 'GH-Hook' -If { ![String]::IsNullOrEmpty($Env:GITHUB_REPOSITORY) -and $TargetObject -is [PSRule.Data.RepositoryInfo] } -Begin {
     Write-Warning "Running convention"
-    Export-GitHubRuleData -UseGitHubToken -OutputPath 'out/repo/repo.json';
-
-    $outputItem = Get-Item -Path 'out/repo/repo.json';
-
-    $PSRule.Import($PSrule.GetContent($outputItem));
+    try {
+        Export-GitHubRuleData -UseGitHubToken -OutputPath 'out/repo/repo.json'
+        #  -Repository $Env:GITHUB_REPOSITORY;
+        $outputItem = Get-Item -Path 'out/repo/repo.json';
+        $PSRule.Import($PSrule.GetContent($outputItem));
+    }
+    catch {
+        Write-Error $_
+    }
 }
