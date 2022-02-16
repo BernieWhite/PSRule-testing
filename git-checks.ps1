b777ddb15db828af7c37f6c75d9ba1ae5a23a733
Bernie White <bewhite@microsoft.com>
2022-02-17 00:15:27 +1000

    Additional tests

--- git-checks.ps1
+++ git-checks.ps1
@@ -9,3 +9,9 @@ git diff --name-only main...HEAD~1
 
 "---"
 
+"Get-Content HEAD"
+Get-Content -Path ./.git/HEAD -ErrorAction Continue;
+
+"dir .git/heads/"
+Get-ChildItem -Path ./.git/heads/ -Recurse -ErrorAction Continue;
+
