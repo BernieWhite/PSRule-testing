b11826da55a7a8116f67e861d84ec8a07133a860
Bernie White <bewhite@microsoft.com>
2022-02-17 00:45:35 +1000

    Bump

--- .ps-rule/Conventions.Rule.ps1
+++ .ps-rule/Conventions.Rule.ps1
@@ -1,5 +1,9 @@
 
 
+Export-PSRuleConvention 'InfoHook' -Initialize {
+    Write-Host "This is a host object"
+}
+
 Export-PSRuleConvention 'GH-Hook' -If { ![String]::IsNullOrEmpty($Env:GITHUB_REPOSITORY) -and $TargetObject -is [PSRule.Data.RepositoryInfo] } -Begin {
     try {
         Export-GitHubRuleData -UseGitHubToken -OutputPath 'out/repo/repo.json';
